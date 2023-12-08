80f800d8cc448c5ae4563464c55935e0a673c683
nwingren <niklas.wingren@eit.lth.se>
2023-12-08 17:58:57 +0100

    Change GPL notice to regular comment

--- fe2ms/systems.py
+++ fe2ms/systems.py
@@ -1,23 +1,23 @@
+# Copyright (C) 2023 Niklas Wingren
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 Finite element boundary integral systems based on different solution approaches.
+
 Currently supported approaches are:
     - Full equation system
     - Adaptive cross approximation (ACA) acceleration of boundary integral part
-
-Copyright (C) 2023 Niklas Wingren
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os as _os
