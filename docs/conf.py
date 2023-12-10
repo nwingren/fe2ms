2544f90c488337e9ea8c3cc8888ec8d191e82413
nwingren <niklas.wingren@eit.lth.se>
2023-12-10 15:49:51 +0100

    Add link to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    # 'sphinx_rtd_theme',
+    'sphinx_rtd_theme',
 ]
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
@@ -31,5 +31,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
